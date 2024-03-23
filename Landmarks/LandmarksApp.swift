d1ff926d7e5139e7132441ae4590b66623b61100
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2024-03-23 19:13:47 +0900

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 川口美咲 on 2024/03/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
